5c371aa883c2e7b2677757af85963dbaf6ece843
g3rion <gerard.kulawik@gmail.com>
2024-05-05 11:27:01 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
 import company.models.Manager;
 import company.models.Worker;
+import company.abstracts.Employee;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Robert Kubica", 15000, 1);
-        Worker worker2 = new Worker("Sergio Perez", 26000, 2);
-        Worker worker3 = new Worker("Max Verstappen", 35000, 3);
-        Worker worker4 = new Worker("Charles Leclerc", 12000, 4);
-        Manager manager = new Manager("Christian Horner", 100000, 5);
+        Worker worker1 = new Worker("Robert Kubica", 15000, 1, "2019-01-01", "Driver");
+        Worker worker2 = new Worker("Sergio Perez", 26000, 2, "2018-02-02", "Driver");
+        Worker worker3 = new Worker("Max Verstappen", 35000, 3, "2017-03-03", "Driver");
+        Worker worker4 = new Worker("Charles Leclerc", 12000, 4, "2016-04-04", "Driver");
+        Manager manager = new Manager("Christian Horner", 100000, 5, "2015-05-05", "Team Manager");
 
-        Worker[] workers = {worker1, worker2, worker3, worker4};
-        for (Worker worker : workers) {
-            System.out.println("Worker's salary: " + worker.getSalary());
-            worker.work();
+        List<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
+
+        for (Employee employee : employees) {
+            employee.work();
+            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() + ", Position: " + employee.getPosition() +
+                    ", Hire date: " + employee.getHireDate() + ", Salary: " + employee.getSalary() + ")");
         }
-
-        System.out.println("Manager's salary: " + manager.getSalary());
-        manager.work();
     }
 }
--- src/company/models/Manager.java
+++ src/company/models/Manager.java
@@ -1,10 +1,11 @@
+// company/models/Manager.java
 package company.models;
 
 import company.abstracts.Employee;
 
 public class Manager extends Employee {
-    public Manager(String name, double salary, int id) {
-        super(name, salary, id);
+    public Manager(String name, double salary, int id, String hireDate, String position) {
+        super(name, salary, id, hireDate, position);
     }
 
     @Override
